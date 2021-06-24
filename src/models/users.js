ec3b853164e834b1c0d670219e0d3455075ccd87
Banke Agrawal <bankebihariagrawal58@gmail.com>
2021-06-24 22:33:15 +0530

    Add new Route users/login

--- src/models/users.js
+++ src/models/users.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-const User = mongoose.model('User', {
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -9,6 +11,7 @@ const User = mongoose.model('User', {
     },
     email: {
         type: String,
+        unique: true,
         trim: true,
         required: true,
         validate(value) {
@@ -38,6 +41,36 @@ const User = mongoose.model('User', {
         }
     }
 })
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+
+// Hash the Plain text password before saving and update
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 // const me = new User({ name: 'Sagar', password: 'sagar@1234   ', email: 'banke@gmail.com' })
 
